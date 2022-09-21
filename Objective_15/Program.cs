efa40351d33740a387f4dec2e3123a4f1eae0db5
BSerge <serge.boskin@icloud.com>
2022-09-21 15:18:10 +0300

    add objective_15

---
+++ Objective_15/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите день недели от 1 до 7: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number < 6)
+
+Console.WriteLine($"{number} рабочий");
+
+else
+
+Console.WriteLine($"{number} выходной");
